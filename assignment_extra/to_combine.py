ea178894ccdcde004baf67482af2688e8a122497
LaiEthanLai <ethanlai1226@gmail.com>
2023-05-07 02:01:50 -0500

    done

--- assignment_extra/to_combine.py
+++ assignment_extra/to_combine.py
@@ -1,2 +1,15 @@
 from PIL import Image
 from pathlib import Path
+
+target = [Path('imagenette2'), Path('imagenette_c'), ]
+head = Path('combine')
+
+for idx, tar in enumerate(target):
+    for i in tar.glob('**/*JPEG'):
+        out = Path('combine')
+        for j in i.parts[1:-1]:
+            out /= j
+        out /= f'{i.stem}_from_{idx}{i.suffix}'
+        Path(out.parent).mkdir(parents=True,exist_ok=True)
+        img = Image.open(i)
+        img.save(out)
